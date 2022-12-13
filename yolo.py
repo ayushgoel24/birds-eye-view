cee4c6072c2428b6d2627c39a43492837b8dbb5f
ayushgoel24 <ayush.goel2427@gmail.com>
2022-12-12 23:16:22 -0500

    multiprocessing errors

--- yolo.py
+++ yolo.py
@@ -1,7 +1,7 @@
 import torchvision.transforms.functional as F
 import numpy as np
 import cv2
-
+from itertools import repeat
 class DrivewayDetection( object ):
 
     def __init__(self) -> None:
@@ -62,10 +62,47 @@ class DrivewayDetection( object ):
         
         return da_seg_pad, ll_seg_pad
 
-    @staticmethod
-    def performDrivewayDetection( yolo_model, img:np.ndarray, yolo_threshold:float, kernel_size:float, out_img_shape=(375, 1242) ) -> tuple:
-        img = DrivewayDetection.transformImage(img)
-        _, da_seg_out,ll_seg_out = yolo_model( img )
-        da_seg, ll_seg = DrivewayDetection.postProcessing( da_seg_out, ll_seg_out, yolo_threshold, kernel_size, out_img_shape )
+    # @staticmethod
+    # def performDrivewayDetectionInternal( yolo_model, imgs:list, yolo_threshold:float, kernel_size:float, counter:int, out_img_shape=(375, 1242) ) -> np.ndarray:
+    #     print("performDrivewayDetectionInternal:", counter)
+    #     img = DrivewayDetection.transformImage(imgs[counter])
+    #     # img = img.to( 'mps' )
+    #     _, da_seg_out,ll_seg_out = yolo_model( img )
+    #     da_seg, ll_seg = DrivewayDetection.postProcessing( da_seg_out.cpu(), ll_seg_out.cpu(), yolo_threshold, kernel_size, out_img_shape )
+    #     da_ll_3 = np.zeros( ( da_seg.shape[0], da_seg.shape[1], 3 ) )
+    #     da_ll_3[:,:,0] = da_seg
+    #     da_ll_3[:,:,1] = ll_seg
+    #     return da_ll_3
 
-        return ( da_seg, ll_seg )
+    @staticmethod
+    def performDrivewayDetectionInternal( yolo_model, img:np.ndarray, yolo_threshold:float, kernel_size:float, out_img_shape=(375, 1242) ) -> np.ndarray:
+        print("performDrivewayDetectionInternal:")
+        img = DrivewayDetection.transformImage(img)
+        img = img.to( 'mps' )
+        _, da_seg_out,ll_seg_out = yolo_model( img )
+        da_seg, ll_seg = DrivewayDetection.postProcessing( da_seg_out.cpu(), ll_seg_out.cpu(), yolo_threshold, kernel_size, out_img_shape )
+        da_ll_3 = np.zeros( ( da_seg.shape[0], da_seg.shape[1], 3 ) )
+        da_ll_3[:,:,0] = da_seg
+        da_ll_3[:,:,1] = ll_seg
+        return da_ll_3
+
+    @staticmethod
+    def performDrivewayDetection( pool, yolo_model, imgs:list, yolo_threshold:float, kernel_size:float, out_img_shape=(375, 1242) ) -> list:
+        print("performDrivewayDetection: ", bool(pool))
+        # da_ll_3s = pool.starmap(
+        #     DrivewayDetection.performDrivewayDetectionInternal, 
+        #     zip(
+        #         repeat(yolo_model), 
+        #         repeat(imgs), 
+        #         repeat(yolo_threshold),
+        #         repeat(kernel_size),
+        #         range(len(imgs)),
+        #         repeat(out_img_shape)
+        #     )
+        # )
+
+        da_ll_3s = []
+        for img in imgs:
+            da_ll_3s.append(DrivewayDetection.performDrivewayDetectionInternal(yolo_model, img, yolo_threshold, kernel_size, out_img_shape))
+        
+        return da_ll_3s
